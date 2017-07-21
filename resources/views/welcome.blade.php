7b471f7de62d64c889940a1d7d22495c255b8269
vagrant <vagrant@homestead>
2017-07-21 03:05:12 +0000

    initial commit after copying from base laravel 5.4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Socialite Tutorials
                 </div>
 
                 <div class="links">
