cfd78154ea99a27294b5b1bfc5483df331875d40
Frederick Sandalo <uscbsitric@gmail.com>
2017-07-25 15:19:45 +0000

    updated the update_users_table migration and ran it

--- database/migrations/2017_07_25_150618_update_users_table.php
+++ database/migrations/2017_07_25_150618_update_users_table.php
@@ -14,7 +14,7 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-          $table->string('users')->nullable()->change();
+          $table->string('email')->nullable()->change();
         });
     }
 
